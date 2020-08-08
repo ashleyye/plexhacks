7d714c30f8f1ce2511c0951bac8a31c240c85f81
ashleyye <37671822+ashleyye@users.noreply.github.com>
2020-08-08 13:45:12 -0400

    updated html 2

--- fileUpload/src/main/java/com/example/fileUpload/FileUploadController.java
+++ fileUpload/src/main/java/com/example/fileUpload/FileUploadController.java
@@ -1,22 +1,22 @@
 package com.example.fileUpload;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
+        import java.io.IOException;
+        import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.core.io.FileSystemResource;
+        import org.springframework.core.io.Resource;
+        import org.springframework.http.HttpHeaders;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.*;
+        import org.springframework.web.multipart.MultipartFile;
+        import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+        import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.fileUpload.storage.StorageFileNotFoundException;
-import com.example.fileUpload.storage.StorageService;
+        import com.example.fileUpload.storage.StorageFileNotFoundException;
+        import com.example.fileUpload.storage.StorageService;
 
 @Controller
 public class FileUploadController {
